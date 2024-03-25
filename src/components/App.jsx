1eb4ea0df1a6d557de4a490630df96087bcbea7f
Shubham75511 <111749753+Shubham75511@users.noreply.github.com>
2024-03-25 10:41:14 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Note/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
