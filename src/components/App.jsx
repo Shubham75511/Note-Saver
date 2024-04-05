34c21bb91e46d190691032b800deaff9b8d4c02e
Shubham75511 <111749753+Shubham75511@users.noreply.github.com>
2024-04-05 13:11:32 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,17 +1,26 @@
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../notes.jsx";
 
-
+function createItem(noteItem){
+  return(
+    <Note 
+    key = {noteItem.id}
+    title = {noteItem.title}
+    content = {noteItem.content}
+     />
+)
+};
 
 function App() {
   return (
     <>
       <Header/>
-      <Note/>
+      {notes.map(createItem)}
       <Footer/>
     </>
   );
 }
 
-export default App
+export default App;
