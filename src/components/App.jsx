127e3492402419fe01160f2d5d0d6c429b65497d
Shubham75511 <111749753+Shubham75511@users.noreply.github.com>
2024-08-13 21:23:34 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,13 @@ function App() {
     });
   }
 
-
+  function deleteNote(id) {
+    setNotes((prevNotes) => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
 
   return (
     <div>
